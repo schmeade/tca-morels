1eb49098b610b9e5879503697c2caacbc766946f
schmeade <157747398+schmeade@users.noreply.github.com>
2026-05-19 13:30:33 -0500

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import { HashRouter, Routes, Route } from "react-router";
 
-function App() {
+const Home = () => <h1>Home</h1>;
+const Setup = () => <h1>Setup</h1>;
+const Play = () => <h1>Play</h1>;
 
+const App = () => {
   return (
     <div>
-        
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
-}
+};
 
 export default App;
