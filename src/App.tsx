f7db7dbf80a48450b774651086b68f1796e6e305
schmeade <157747398+schmeade@users.noreply.github.com>
2026-05-18 20:09:36 -0500

    Install and tested Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1
+            className='text-3xl font-bold'
+          >Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
